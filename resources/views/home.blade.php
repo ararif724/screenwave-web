c7bd780577dd90e429c470ea34b82cb028bd7716
Arifur Rahman Arif <ararif724@gmail.com>
2024-07-04 17:36:47 +0600

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    @viteReactRefresh
-    @vite('resources/js/app.jsx')
 </head>
 
 <body>
